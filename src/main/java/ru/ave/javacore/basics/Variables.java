4a7e0c1df9840586e35a37fc0f8d159b9ea67c4d
Alexey <ya@home.ru>
2026-05-22 22:18:02 +0500

    compete_first_lesson

--- src/main/java/ru/ave/javacore/basics/Variables.java
+++ src/main/java/ru/ave/javacore/basics/Variables.java
@@ -1,4 +1,16 @@
 package ru.ave.javacore.basics;
 
 public class Variables {
+
+    public static void main(String[] args) {
+        int i = 5;
+        double d = 4.56;
+        boolean b = true;
+        char c = 'e';
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
 }
