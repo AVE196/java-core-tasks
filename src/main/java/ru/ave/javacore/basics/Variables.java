346c373ffd69011ea2cc50189cd408a6deaaa84e
Alexey <ya@home.ru>
2026-05-31 19:21:01 +0500

    fix pr

--- src/main/java/ru/ave/javacore/basics/Variables.java
+++ src/main/java/ru/ave/javacore/basics/Variables.java
@@ -1,16 +1,24 @@
 package ru.ave.javacore.basics;
 
 public class Variables {
-
     public static void main(String[] args) {
-        int i = 5;
-        double d = 4.56;
-        boolean b = true;
-        char c = 'e';
-        System.out.println(i);
-        System.out.println(d);
-        System.out.println(b);
-        System.out.println(c);
+        int[] numbers = {1,5,8,7};
+        sorting(numbers);
+        for(int i : numbers) {
+            System.out.println(i);
+        }
+    }
+
+    static void sorting(int[] num) {
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < num.length; j++) {
+                if (num[i] <= num[j]) {
+                    int temp = num[j];
+                    num[j] = num[i];
+                    num[i] = temp;
+                }
+            }
+        }
     }
 
 }
